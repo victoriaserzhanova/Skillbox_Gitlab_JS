f965381a67809b94b1ed7b2b31655e71e30eb155
Сержанова Виктория <victoriaserzhanova@gmail.com>
2020-11-22 14:04:32 +0000

    Update main.js

--- 06_objects/06_HW/main.js
+++ 06_objects/06_HW/main.js
@@ -1,31 +1,39 @@
 'use strict';
 
-let valuesForDropdownList =[
+const attributesForOption =[
 	{ value: 'value_1', label: 'label_1'},
 	{ value: 'value_2', label: 'label_2'},
 ]
 
-function createDropdownList(array, indexDefault = 0) {
-    let dropdownList = document.createElement('select');
+const valueDefault = 'value_1';
 
-	array.forEach((item, index) => {
-		let dropdownListItem = document.createElement('option');
-
-		dropdownListItem.value = item['value'];
-		dropdownListItem.textContent = item['label'];
-		dropdownList.append(dropdownListItem);
-
-		if (index === 0) {
-			dropdownListItem.setAttribute('selected', true);
-		}
-
-		if (index === indexDefault) {
-			dropdownListItem.setAttribute('selected', true);
-		}
-	});
-	return dropdownList;
+function createElem(tagName, content, object) {
+	const newEl = document.createElement(tagName);
+	newEl.textContent = content;
+	for (let key in object) {
+		newEl.setAttribute( key, object[key])
+	}
+	return newEl;
 }
 
-let dropdownList = createDropdownList(valuesForDropdownList);
+function createDropdownListElem(array, valueDefault) {
+	const content = 'text content';
+	const dropdownListEl = createElem('select', content);
+	array.forEach((item) =>{
+		const optionEl = createElem('option', content, item);
+
+		if (optionEl.value === valueDefault) {
+			optionEl.setAttribute('selected', true);
+		}
+		dropdownListEl.append(optionEl);
+	})
+	return dropdownListEl;
+}
+
+const dropdownListEl = createDropdownListElem(attributesForOption, valueDefault);
+
+
+
+
 
 
