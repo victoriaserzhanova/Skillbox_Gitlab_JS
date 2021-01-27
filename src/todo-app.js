2f653d73f5781f6558badddfa35f5b354a5a7cf8
Victoria Serzhanova <victoriaserzhanova@gmail.com>
2021-01-27 13:28:01 +0300

    add functions:addItemInLS;changeItemPropertyInLs

--- src/todo-app.js
+++ src/todo-app.js
@@ -8,172 +8,225 @@
     };
     const currentStorageKey = storageKeys[userName];
 
-    let todoAppTitle = createAppTitle(title);
-    let itemForm = createItemForm();
-    let todoList = createTodoList();
-    let todoListData = [];
-    console.log(todoListData);
+    let appTitleEl = createTitle(title);
+    let itemFormEl= createItemForm();
+    let todoListEl = createTodoList();
+    let itemsData = [];
 
-    container.append(todoAppTitle);
-    container.append(itemForm.form);
-    container.append(todoList);
+    container.append(appTitleEl);
+    container.append(itemFormEl.formEl);
+    container.append(todoListEl);
 
-    itemForm.formBtn.setAttribute("disabled", "");
+    itemFormEl.formBtnEl.setAttribute("disabled", "");
 
-    if (localStorage) {
-      addTodoListFromLocalstorage(todoList, currentStorageKey, todoListData);
-    }
+    addTodoListFromLS(todoListEl, currentStorageKey, itemsData);
 
-    // set attribute disabled for formBtn if formInput is empty
-    itemForm.formInput.addEventListener("input", function () {
-      if (itemForm.formInput.value) {
-        itemForm.formBtn.removeAttribute("disabled");
+    // set attribute disabled for formBtnEl if formInputEl is empty
+    itemFormEl.formInputEl.addEventListener("input", function () {
+      if (itemFormEl.formInputEl.value) {
+        itemFormEl.formBtnEl.removeAttribute("disabled");
       } else {
-        itemForm.formBtn.setAttribute("disabled", "");
+        itemFormEl.formBtnEl.setAttribute("disabled", "");
       }
     });
 
-    // form submit event creates new item
-    // browser creates event submit on form when user presses 'add task' btn or ENTER
-    itemForm.form.addEventListener("submit", function (event) {
-      // prevent default behaviour like page reloading after form submit
+    // formElsubmit event creates new item
+    itemFormEl.formEl.addEventListener("submit", function (event) {
+      // prevent default behaviour like page reloading after formElsubmit
       event.preventDefault();
 
-      // NO form submitting if input value is null
-      if (!itemForm.formInput.value) {
+      // NO formElsubmitting if input value is null
+      if (!itemFormEl.formInputEl.value) {
         return false;
       }
 
       // create and add new list-item to todo list. list-item name == input
-      let itemData = {
-        name: itemForm.formInput.value,
+      let listItemData = {
+        name: itemFormEl.formInputEl.value,
         done: false,
       };
-      let listItem = createItem(itemData, todoListData, currentStorageKey);
-      todoList.append(listItem.item);
+
+      let listItem = createItem(listItemData, itemsData, currentStorageKey);
+      todoListEl.append(listItem.itemEl);
+
+      addItemInLS(listItemData, itemsData, currentStorageKey);
 
       // clean input
-      itemForm.formInput.value = "";
+      itemFormEl.formInputEl.value = "";
 
-      itemForm.formBtn.setAttribute("disabled", "");
+      itemFormEl.formBtnEl.setAttribute("disabled", "");
       return listItem;
     });
-
-
-    // delBtn.addEventListener("click", deleteBtnClickHandler);
-
   };
 
   // create and return app title
-  function createAppTitle(title) {
-    let appTitle = document.createElement("h2");
-    appTitle.textContent = title;
-    return appTitle;
+  function createTitle(title) {
+    let titleEl = document.createElement("h2");
+    titleEl.textContent = title;
+    return titleEl;
   }
 
-  // create and return OBJECT containing form, input and btn
+  // create and return OBJECT containing formEl, input and btn
   function createItemForm() {
-    let formInput = document.createElement("input");
-    let form = document.createElement("form");
-    let formBtn = document.createElement("button");
-    let formBtnWrapper = document.createElement("div");
+    let formInputEl = document.createElement("input");
+    let formEl= document.createElement("form");
+    let formBtnEl = document.createElement("button");
+    let formBtnWrapperEl = document.createElement("div");
 
-    form.classList.add("input-group", "mb-3");
-    formInput.classList.add("form-control");
-    formInput.placeholder = "Enter new task";
-    formBtnWrapper.classList.add("input-group-append");
-    formBtn.classList.add("btn", "btn-primary");
-    formBtn.textContent = "Add task";
+    formEl.classList.add("input-group", "mb-3");
+    formInputEl.classList.add("form-control");
+    formInputEl.placeholder = "Enter new task";
+    formBtnWrapperEl.classList.add("input-group-append");
+    formBtnEl.classList.add("btn", "btn-primary");
+    formBtnEl.textContent = "Add task";
 
-    formBtnWrapper.append(formBtn);
-    form.append(formInput);
-    form.append(formBtnWrapper);
+    formBtnWrapperEl.append(formBtnEl);
+    formEl.append(formInputEl);
+    formEl.append(formBtnWrapperEl);
 
     return {
-      form,
-      formInput,
-      formBtn,
+      formEl,
+      formInputEl,
+      formBtnEl,
     };
   }
 
   // create and return todo LIST
   function createTodoList() {
-    let list = document.createElement("ul");
-    list.classList.add("list-group");
-    return list;
+    let listEl = document.createElement("ul");
+    listEl.classList.add("list-group");
+    return listEl;
   }
 
   // create and return OBJECT, containing list-item, delete and done btns
-  function createItem(object,todoListData,currentStorageKey) {
-    let item = document.createElement("li");
+  function createItem(object, itemsData, currentStorageKey) {
+    let itemEl = document.createElement("li");
     // create button group, containing done and delete btns
-    let btnGroup = document.createElement("div");
-    let doneBtn = document.createElement("button");
-    let delBtn = document.createElement("button");
+    let btnGroupEl = document.createElement("div");
+    let doneBtnEl = document.createElement("button");
+    let delBtnEl = document.createElement("button");
 
     // set Bootstrap classes for list-item and btns
-    item.classList.add(
+    itemEl.classList.add(
       "list-group-item",
       "d-flex",
       "justify-content-between",
       "align-items-center"
     );
-    btnGroup.classList.add("btn-group", "btn-group-sm");
-    doneBtn.classList.add("btn", "btn-success");
-    delBtn.classList.add("btn", "btn-danger");
+    btnGroupEl.classList.add("btn-group", "btn-group-sm");
+    doneBtnEl.classList.add("btn", "btn-success");
+    delBtnEl.classList.add("btn", "btn-danger");
 
-    item.textContent = object.name;
-    doneBtn.textContent = "Done";
-    delBtn.textContent = "Delete";
+    itemEl.textContent = object.name;
+    doneBtnEl.textContent = "Done";
+    delBtnEl.textContent = "Delete";
 
     // add btns to one block
-    btnGroup.append(doneBtn);
-    btnGroup.append(delBtn);
-    item.append(btnGroup);
+    btnGroupEl.append(doneBtnEl);
+    btnGroupEl.append(delBtnEl);
+    itemEl.append(btnGroupEl);
 
     if (object.done == true) {
-      item.classList.add("list-group-item-success");
+      itemEl.classList.add("list-group-item-success");
     }
 
-    todoListData.push(object);
-    console.log(todoListData);
-    localStorage.setItem(currentStorageKey, JSON.stringify(todoListData));
-    console.log(localStorage);
+    doneBtnEl.addEventListener("click", function() {
+      itemEl.classList.toggle("list-group-item-success");
 
-    doneBtn.addEventListener("click", function() {
-        item.classList.toggle("list-group-item-success");
-        if (item.classList.contains("list-group-item-success")) {
-            object.done = true;
-        } else {
-            object.done = false;
-        }
-        console.log(todoListData);
-        localStorage.setItem(currentStorageKey, JSON.stringify(todoListData));
-        console.log(localStorage);
-        });
-
-    delBtn.addEventListener("click", function() {
-      item.remove();
-      object.name = '';
-
-      localStorage.setItem(currentStorageKey, JSON.stringify(todoListData));
+      changeItemPropertyInLs(object,itemsData, currentStorageKey);
       });
+
+    delBtnEl.addEventListener("click", function() {
+      if (confirm('Delete task')) {
+        itemEl.remove();
+        deleteItemFromLs(object,itemsData, currentStorageKey);
+        console.log(localStorage)
+      };
+      console.log(localStorage)
+    });
 
     // app needs access to item and btns
     return {
-      item,
-      doneBtn,
-      delBtn,
+      itemEl,
+      doneBtnEl,
+      delBtnEl,
     };
   }
 
-  function addTodoListFromLocalstorage(todoList,currentStorageKey, todoListData) {
-    let storageTodoList = JSON.parse(localStorage.getItem(currentStorageKey));
-    if (storageTodoList) {
+  function addItemInLS(object,arrayOfObjects, currentStorageKey) {
+      if (getItemsFromLs(currentStorageKey)){
+        arrayOfObjects = getItemsFromLs(currentStorageKey);
+      }
+      if (countItemsInLs(currentStorageKey)) {
+        object.id = countItemsInLs(currentStorageKey);
+      }else{
+        object.id = 0;
+      }
+      arrayOfObjects.push(object);
+      localStorage.setItem(
+        currentStorageKey,
+        JSON.stringify(arrayOfObjects)
+      );
+    }
+
+  function changeItemPropertyInLs(object,arrayOfObjects, currentStorageKey) {
+      arrayOfObjects = getItemsFromLs(currentStorageKey);
+      let itemToChangeId = object.id;
+      let itemToChange = arrayOfObjects.find(item => item.id == itemToChangeId);
+      if (itemToChange.done == true) {
+        itemToChange.done = false;
+      }else {
+        itemToChange.done = true;
+      }
+      console.log(arrayOfObjects)
+      localStorage.setItem(
+        currentStorageKey,
+        JSON.stringify(arrayOfObjects)
+      );
+      console.log(localStorage)
+  }
+
+  function deleteItemFromLs(object,arrayOfObjects, currentStorageKey) {
+    arrayOfObjects = getItemsFromLs(currentStorageKey);
+    let itemId = object.id;
+    for (let i=0; i<arrayOfObjects.length; i++) {
+      let indexOfItemToChange = i;
+      if (arrayOfObjects[i].id == itemId) {
+        arrayOfObjects.splice(indexOfItemToChange,1);
+        localStorage.setItem(
+          currentStorageKey,
+          JSON.stringify(arrayOfObjects)
+        );
+      }
+    }
+  }
+
+  function getItemsFromLs(currentStorageKey) {
+      if (localStorage.getItem(currentStorageKey)) {
+          let items = JSON.parse(localStorage.getItem(currentStorageKey));
+          return items;
+      } else {
+          return false;
+      }
+  }
+
+  function countItemsInLs(currentStorageKey) {
+      if (getItemsFromLs(currentStorageKey)) {
+          return getItemsFromLs(currentStorageKey).length;
+      } else {
+          return false;
+      }
+  }
+
+  function addTodoListFromLS(todoListEl, currentStorageKey, itemsData) {
+    if (getItemsFromLs(currentStorageKey)) {
+      let storageTodoList = getItemsFromLs(currentStorageKey);
       storageTodoList.forEach(function (element) {
-        let listItemFromLocalstorage = createItem(element, todoListData,currentStorageKey);
-        todoList.append(listItemFromLocalstorage.item);
+        let listItemFromLS = createItem(element, itemsData,currentStorageKey);
+        todoListEl.append(listItemFromLS.itemEl);
       });
+    }else {
+      return false;
     }
   }
 
